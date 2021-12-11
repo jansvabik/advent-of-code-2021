c0e950aeb72d20997e1551edd2bcb9ec4df8be46
Jan Švábík <jansvabik@jansvabik.cz>
2021-12-11 15:07:05 +0100

    feat: add day 10

--- internal/day10/day10.go
+++ internal/day10/day10.go
@@ -1,51 +1,87 @@
 package day10
 
 import (
-	"strconv"
-	"strings"
+	"sort"
 )
-
-// This structure is different for each day and should be optimized for storing
-// the input data in a way achieving simple puzzle solving afterwards.
-type inputPart struct {
-	value int
-}
 
 // Variable input obviously contains the day input which one should pass to the
 // program using stdin pipe in console (./program <day> < input.txt). The input
 // string is already processed by this day function (RegisterInput).
-var input []inputPart
+var inputs []string
+
+// The table of points of the closing brackets.
+var points = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+// The table of points of the closing brackets.
+var autocompletePoints = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
+var oppositeBracket = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+	'<': '>',
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<',
+}
+
+var autocompletePointsList = []int{}
 
 // RegisterInput processes each line of the input and stores it in prepared
 // input variables above. It does the check of each line to match the day
 // requirements.
 func RegisterInput(data string) error {
-	// split the string to the command and its valud
-	split := strings.Split(data, " ")
-
-	// try to convert the string to the integer
-	num, err := strconv.Atoi(split[1])
-	if err != nil {
-		return err
-	}
-
-	// store the number in input var
-	input = append(input, inputPart{
-		value: num,
-	})
+	inputs = append(inputs, data)
 	return nil
 }
 
 // Part 1 solves the puzzle itself and returns the result. It calls other
 // internal helper functions to get the result. It solves the first part.
 func Part1() (interface{}, error) {
-	result := 0
-	return result, nil
+	penaltyPoints := 0
+	for _, line := range inputs {
+		var openedBrackets []rune
+		for _, b := range line {
+			if b == '(' || b == '[' || b == '{' || b == '<' {
+				openedBrackets = append(openedBrackets, b)
+			} else {
+				if openedBrackets[len(openedBrackets)-1] != oppositeBracket[b] {
+					penaltyPoints += points[b]
+					openedBrackets = make([]rune, 0)
+					break
+				} else {
+					openedBrackets = openedBrackets[:len(openedBrackets)-1]
+				}
+			}
+		}
+
+		// incomplete line autocomplete points calculation
+		if len(openedBrackets) > 0 {
+			acPoints := 0
+			for i := len(openedBrackets) - 1; i >= 0; i-- {
+				acPoints *= 5
+				acPoints += autocompletePoints[oppositeBracket[openedBrackets[i]]]
+			}
+			autocompletePointsList = append(autocompletePointsList, acPoints)
+		}
+	}
+	return penaltyPoints, nil
 }
 
 // Part 2 solves the puzzle itself and returns the result. It calls other
 // internal helper functions to get the result. It solves the second part.
 func Part2() (interface{}, error) {
-	result := 0
-	return result, nil
+	sort.Ints(autocompletePointsList)
+	return autocompletePointsList[len(autocompletePointsList)/2], nil
 }
